7eb409398e5d3e7108fd417ba924621d3d687a99
Zeeshan <zeeshanawan1998@gmail.com>
2022-06-28 18:13:27 +0500

    added curl get request code in search function

--- src/SrMetaExtractor.php
+++ src/SrMetaExtractor.php
@@ -6,17 +6,49 @@ class SrMetaExtractor
 {
 
     private $searchEngine;
+    private $resultCollection=[];
 
-
+    /*
+     * setSearchEngine used to set search engine by user
+     *
+     */
     public function setSearchEngine($engine = "google.com")
     {
         $this->searchEngine=$engine;
     }
 
+    /*
+     * search function keywords will be passed in array to search query on specified search engine
+     */
+
     public function search(array $keywords){
-//        for each loop for keywords array
-        $query=urlencode($keywords);
-        $url = 'https://'.$this->searchEngine.'/search?q='.$query;
-        $scrape = file_get_contents($url);
+
+
+        //for each loop for keywords array
+        $curl= curl_init();
+
+        foreach($keywords as $keyword){
+
+            $query=urlencode($keyword);
+            curl_setopt($curl, CURLOPT_URL, 'https://'.$this->searchEngine.'/search?q='.$query);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            $resp = curl_exec($curl);
+            array_push($this->resultCollection,$resp);
+        }
+
+        curl_close($curl);
+
+        return $this->resultCollection;
     }
+
+
+
+
+
+
 }
+
+
+//Notes
+// $scrape = file_get_contents($url);
+//$url = 'https://'.$this->searchEngine.'/search?q='.$query;
