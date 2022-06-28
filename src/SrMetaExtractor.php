474bd1385cfb4dd137bc42b6c6fa6a29feaf5e91
Zeeshan <zeeshanawan1998@gmail.com>
2022-06-28 17:32:48 +0500

    first comment

---
+++ src/SrMetaExtractor.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Zeeshan\SrMetaExtractor;
+
+class SrMetaExtractor
+{
+
+    private $searchEngine;
+
+
+    public function setSearchEngine($engine = "google.com")
+    {
+        $this->searchEngine=$engine;
+    }
+
+    public function search(array $keywords){
+//        for each loop for keywords array
+        $query=urlencode($keywords);
+        $url = 'https://'.$this->searchEngine.'/search?q='.$query;
+        $scrape = file_get_contents($url);
+    }
+}
