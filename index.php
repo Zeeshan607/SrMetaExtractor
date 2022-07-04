d37b007faeb8b04bdd3f1324af9ff7b07f2ae33c
Zeeshan <zeeshanawan1998@gmail.com>
2022-07-04 18:13:44 +0500

    improved commenting in code for better understanding  of logic

--- index.php
+++ index.php
@@ -4,18 +4,18 @@ require_once './vendor/autoload.php';
 use Fynda\SrMetaExtractor\SrMetaExtractor;
 
 
-try{
-    $me= new SrMetaExtractor();
+try {
+    $me = new SrMetaExtractor();
     $me->setSearchEngine();// default search engin is google.com
-    $data= $me->search(["top hosting websites","best trading websites"]);
+    $data = $me->search(["top hosting websites", "best trading websites"]);
 
-    foreach($data as $keywords){
-        foreach($keywords as $sResult){
-            echo $sResult->meta->title ."</br>";
+    foreach ($data as $keywords) {
+        foreach ($keywords as $sResult) {
+            echo $sResult->meta->title . "</br>";
         }
     }
 
-}catch (\Exception $ex){
+} catch (\Exception $ex) {
     echo $ex->getMessage();
 }
 
--- src/SrMetaExtractor.php
+++ src/SrMetaExtractor.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace  Fynda\SrMetaExtractor;
+namespace Fynda\SrMetaExtractor;
 
 
 use DOMXPath;
@@ -10,7 +10,7 @@ class SrMetaExtractor
 {
 
     private $searchEngine;
-    private $resultCollection=[];
+    private $resultCollection = [];
 
     /*
      * setSearchEngine used to set search engine by user
@@ -18,48 +18,53 @@ class SrMetaExtractor
      */
     public function setSearchEngine($engine = "google.com")
     {
-        $this->searchEngine=$engine;
+        $this->searchEngine = $engine;
     }
 
     /*
      * search function keywords will be passed in array to search query on specified search engine
      */
 
-    public function search(array $keywords){
+    public function search(array $keywords)
+    {
 
 
-//  initiating curl and checking for error;
-        $curl= curl_init();
+        //  initiating curl and checking for error;
+        $curl = curl_init();
         if ($curl === false) {
-                throw new \Exception("Unable to initialize curl");
+            throw new \Exception("Unable to initialize curl");
         }
 
         //for each loop for keywords array
-        foreach($keywords as $kIndex=>$keyword) {
+        foreach ($keywords as $kIndex => $keyword) {
 
-            $finalKeywordResult=[];
+            $keywordFinalResult = [];//array for storing results for single keyword
             $query = urlencode($keyword);
             curl_setopt($curl, CURLOPT_URL, 'https://www.' . $this->searchEngine . '/search?q=' . $query);
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
             $resultPage = curl_exec($curl);
-
+            //checking for curl_exec  output errors
             if (curl_errno($curl) || $resultPage == false) {
                 throw new \Exception(curl_error($curl));
             }
-
+            // initiating DOMDocument
             $dom = new \DOMDocument();
             @$dom->loadHTML($resultPage);
+            //initiating DOMXPath
             $xpath = new DOMXPath($dom);
 //            loop for getting promoted sites
             foreach ($xpath->query('//div[@class="uEierd"]//div[@class="Gx5Zad fP1Qef EtOod pkphOe"]') as $key => $pSite) {
+                // creating object for single result
                 $keywordSearchResult = new stdClass();
                 $keywordSearchResult->keyword_name = $keyword;
                 $keywordSearchResult->rank = $key;
+                // creating nested object named meta for metadata separation
                 $keywordSearchResult->meta = new stdClass();
+                //foreach loop for mapping child nodes of current promoted site block
                 foreach ($pSite->childNodes as $node) {
-
+                    // if condition for getting result's title, link and promoted status
                     if ($node->getAttribute("class") == "v5yQqb jqWpsc" && $node->firstChild->getAttribute("role") == "presentation") {
                         //  url of site
                         $keywordSearchResult->meta->url = $node->firstChild->getAttribute("href");
@@ -68,22 +73,26 @@ class SrMetaExtractor
                         // bool For [Add or normal] site link
                         $keywordSearchResult->meta->promoted = 1;
                     }
+                    // if condition for getting description of promoted site
                     if ($node->getAttribute('class') == "w1C3Le" && $node->firstChild->getAttribute("class") == "MUxGbd yDYNvb lyLwlc") {
                         $keywordSearchResult->meta->description = $node->firstChild->textContent;
                     }
                 }
-                array_push($finalKeywordResult, $keywordSearchResult);
+                // pushing promoted sites data object into $keywordFinalResult array
+                array_push($keywordFinalResult, $keywordSearchResult);
             }
 
 //            loop for getting Normal sites
             foreach ($xpath->query('//div[@class="Gx5Zad fP1Qef xpd EtOod pkphOe"]') as $key => $site) {
-
+                // creating object for single result
                 $keywordSearchResult = new stdClass();
                 $keywordSearchResult->keyword_name = $keyword;
                 $keywordSearchResult->rank = $key;
+                // creating nested object named meta for metadata separation
                 $keywordSearchResult->meta = new stdClass();
+                //foreach loop for mapping child nodes of current normal site block
                 foreach ($site->childNodes as $node) {
-
+                    // if condition for getting result's title, link and promoted status
                     if ($node->getAttribute("class") == "egMi0 kCrYT") {
                         //  url of site
                         $keywordSearchResult->meta->url = "www." . $this->searchEngine . $node->firstChild->getAttribute("href");
@@ -92,23 +101,24 @@ class SrMetaExtractor
                         // bool For [Add or normal] site link
                         $keywordSearchResult->meta->promoted = 0;
                     }
+                    // if condition for getting description of normal site
                     if ($node->getAttribute('class') == "kCrYT") {
 
                         $keywordSearchResult->meta->description = $xpath->query('*/div[@class="BNeawe s3v9rd AP7Wnd"]', $node)->item(0)->textContent;
                     }
                 }
-                array_push($finalKeywordResult, $keywordSearchResult);
+                // pushing normal sites data object into $keywordFinalResult array
+                array_push($keywordFinalResult, $keywordSearchResult);
             }
-
-            $this->resultCollection[$kIndex]=$finalKeywordResult;
+            // storing a $kIndex keyword's results array into collection array
+            $this->resultCollection[$kIndex] = $keywordFinalResult;
         }
 
 
         curl_close($curl);
 
-        return $this->resultCollection ;
+        return $this->resultCollection;
     }
-
 
 
 }
